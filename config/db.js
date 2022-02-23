acb9a63e7f6e6bb6fa7e115405c61e387e4d39ee
idangut <agutierrez@imetrix.com.co>
2022-02-23 18:47:23 -0500

    Cambios en la conexion

--- config/db.js
+++ config/db.js
@@ -4,16 +4,48 @@ const mongoose = require('mongoose');
 
 
 
-function makeNewConnection(dbname) {
-
-    const db = mongoose.createConnection(process.env.STRING_CONNECTION,{
+async function makeNewConnection(dbname) {
+    return new Promise (function(resolve, reject){
+        const db = mongoose.createConnection(process.env.STRING_CONNECTION,{
+            /* bufferCommands: false, // Disable mongoose buffering
+            bufferMaxEntries: 0, */
+            dbName:dbname,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            //useCreateIndex:true,
+            tls: true,
+            tlsCAFile: `${__dirname}/../${process.env.CERTIFICATE}`
+    
+        });
+    
+        db.on('error', function (error) {
+            /* console.log(`MongoDB :: connection ${this.name} ${JSON.stringify(error)}`); */
+            reject(error)
+            /* db.close().catch(() => console.log(`MongoDB :: failed to close connection ${this.name}`)); */
+        });
+    
+    
+        db.on('connected', function () {
+            console.log(`MongoDB :: connected ${this.name}`);
+            resolve(db)
+            /* mongoose.set('debug', function (col, method, query, doc) {
+                console.log(`MongoDB :: ${this.conn.name} ${col}.${method}(${JSON.stringify(query)},${JSON.stringify(doc)})`);
+            });
+            console.log(`MongoDB :: connected ${this.name}`); */
+        });
+    
+        /* db.on('disconnected', function () {
+            console.log(`MongoDB :: disconnected ${this.name}`);
+        });
+    
+        return db; */
+    })
+/*     return await mongoose.createConnection(process.env.STRING_CONNECTION,{
+        bufferCommands: false, // Disable mongoose buffering
+        bufferMaxEntries: 0,
         dbName:dbname,
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        keepAlive: true,
-        
-        connectTimeoutMS:  5000,
-        socketTimeoutMS:  120000,
         //useCreateIndex:true,
         tls: true,
         tlsCAFile: `${__dirname}/../${process.env.CERTIFICATE}`
@@ -33,13 +65,12 @@ function makeNewConnection(dbname) {
         console.log(`MongoDB :: connected ${this.name}`);
         
     });
-    
 
     db.on('disconnected', function () {
         console.log(`MongoDB :: disconnected ${this.name}`);
     });
 
-    return db;
+    return db; */
 
 }
 
